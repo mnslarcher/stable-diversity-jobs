64a0eb7bbb2e32fee9bd6312450bc983706fd943
Mario Namtao Shianti Larcher <mnslarcher@gmail.com>
2023-11-12 21:34:51 +0100

    Generate text and detailed text

--- generate_prompts_csv.py
+++ generate_prompts_csv.py
@@ -16,6 +16,13 @@ def parse_args():
         description="Generate a CSV file with image-text pairs for dataset preparation."
     )
     parser.add_argument(
+        "-r",
+        "--repeat_prompt",
+        type=int,
+        default=10,
+        help="Number of times to repeat each prompt.",
+    )
+    parser.add_argument(
         "-o",
         "--output_dir",
         type=str,
@@ -37,39 +44,50 @@ def load_yaml(file_name):
         return yaml.safe_load(file)
 
 
-def create_prompt(ethnicity, gender_appearance, job, adjectives):
-    adjective_str = ", ".join(adjectives)
-    return f"A professional photo of a {ethnicity} {gender_appearance} {job}, {adjective_str}"
+def create_prompt(job, adjectives):
+    prompt = f"A professional photo of {job}, {adjectives}"
+    return prompt
+
+
+def create_detailed_prompt(ethnicity, gender_appearance, job, adjectives):
+    job = " ".join(job.split(" ")[1:])  # Remove a/an
+    prompt = (
+        f"A professional photo of {ethnicity} {gender_appearance} {job}, {adjectives}"
+    )
+
+    return prompt
 
 
 def main(args):
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    ethnicities = load_yaml("yamls/ethnicities.yaml")
-    gender_appearances = load_yaml("yamls/gender-appearances.yaml")
-    jobs_dict = load_yaml("yamls/jobs.yaml")
+    ethnicities = load_yaml("prompt_parameters/ethnicities.yaml")
+    gender_appearances = load_yaml("prompt_parameters/gender-appearances.yaml")
+    jobs_dict = load_yaml("prompt_parameters/jobs.yaml")
     jobs = jobs_dict["female"] + jobs_dict["male"]
-    adjectives = load_yaml("yamls/adjectives.yaml")
+    adjectives = ", ".join(load_yaml("prompt_parameters/adjectives.yaml"))
 
     output_file_path = output_dir / args.output_file
 
     # Generate prompts and write to a CSV file
     with open(output_file_path, "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(["image", "text"])  # Column headers
+        writer.writerow(["image", "text", "detailed_text"])  # Column headers
 
         image_id = 1
 
         for ethnicity in ethnicities:
             for gender_appearance in gender_appearances:
                 for job in jobs:
-                    prompt = create_prompt(
+                    simple_prompt = create_prompt(job, adjectives)
+                    prompt = create_detailed_prompt(
                         ethnicity, gender_appearance, job, adjectives
                     )
-                    file_name = f"image_{image_id}.png"
-                    writer.writerow([file_name, prompt])
-                    image_id += 1
+                    for _ in range(args.repeat_prompt):
+                        file_name = f"image_{image_id}.png"
+                        writer.writerow([file_name, simple_prompt, prompt])
+                        image_id += 1
 
     logging.info(f"CSV file has been created successfully at {output_file_path}.")
 
